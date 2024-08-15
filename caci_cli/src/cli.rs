d1e778a8a6ff40025abd63cbdc16e10b13f2952f
declspecl <gavind2559@gmail.com>
2024-08-14 23:00:26 -0400

    [cli] add local and remote hook definitions and implementation

--- caci_cli/src/cli.rs
+++ caci_cli/src/cli.rs
@@ -36,14 +36,60 @@ pub enum CaciCommands {
 
 #[derive(Debug, Subcommand)]
 pub enum CaciHookCommands {
-    Add {
-        #[arg(short, long, value_name = "HOOK_NAME")]
-        name: String,
-        #[arg(short, long, value_name = "HOOK_COMMAND")]
-        command: String
-    },
+    /// Add new local or remote hooks
+    #[command(subcommand)]
+    Add(CaciHookAddCommands),
+    /// Remove a hook by name
     Remove {
-        #[arg(short, long, value_name = "HOOK_NAME")]
+        #[arg(short, long, value_name = "NAME")]
         name: String
+    }
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum CaciHookStage {
+    PreCommit,
+    PrepareCommitMsg,
+    CommitMsg,
+    PostCommit,
+    PrePush
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum CaciHookOutput {
+    Stdout,
+    Commit,
+    Silent
+}
+
+#[derive(Debug, Subcommand)]
+pub enum CaciHookAddCommands {
+    /// Add a new local hook (defined local script)
+    Local {
+        #[arg(short, long, value_name = "NAME")]
+        name: String,
+        #[arg(short, long, value_name = "DESCRIPTION")]
+        description: Option<String>,
+        #[arg(short, long, value_name = "COMMAND")]
+        command: String,
+        #[arg(short, long, value_name = "STAGE")]
+        stage: CaciHookStage,
+        #[arg(short, long, value_name = "OUTPUT", default_value = "stdout")]
+        output: CaciHookOutput
+    },
+    /// Add a new remote hook (pull script from URL and execute)
+    Remote {
+        #[arg(short, long, value_name = "NAME")]
+        name: String,
+        #[arg(short, long, value_name = "DESCRIPTION")]
+        description: Option<String>,
+        #[arg(short, long, value_name = "HOOK_URL")]
+        hook_url: String,
+        #[arg(short, long, value_name = "HOOK_EXECUTOR")]
+        hook_executor: String,
+        #[arg(short, long, value_name = "STAGE")]
+        stage: CaciHookStage,
+        #[arg(short, long, value_name = "OUTPUT", default_value = "stdout")]
+        output: CaciHookOutput
     }
 }
