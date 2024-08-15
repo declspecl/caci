a5b44594679dc1bda8c815dd7ad83967f66a8c71
declspecl <gavind2559@gmail.com>
2024-08-14 22:14:53 -0400

    [cli] add skeleton logic for vcs agent

--- caci_cli/src/main.rs
+++ caci_cli/src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use caci_core::CaciResult;
 use caci_fs::{config::CaciConfig, git::GitCaciFilesystemAgent, native::NativeCaciFilesystemAgent, CaciFilesystemAgent};
@@ -10,70 +10,68 @@ pub mod cli;
 fn main() -> CaciResult<()> {
     let args = CaciCli::parse();
 
-    let caci_fs_agent: Box<dyn CaciFilesystemAgent> = match args.command {
-        CaciCommands::New { project_name, agent } => {
-            let project_name = PathBuf::from(project_name);
-            let caci_config = CaciConfig::new(match agent {
-                cli::CaciVcsAgent::Git => caci_fs::config::CaciVcsAgent::Git,
-                cli::CaciVcsAgent::Native => caci_fs::config::CaciVcsAgent::Native
-            });
-
+    let caci_config = match args.command {
+        CaciCommands::New { project_name: _, ref agent } => {
             match agent {
-                CaciVcsAgent::Git => {
-                    Box::new(GitCaciFilesystemAgent::new(project_name));
+                cli::CaciVcsAgent::Git => {
+                    CaciConfig::new(caci_fs::config::CaciVcsAgent::Git)
                 },
-                CaciVcsAgent::Native => {
-                    Box::new(NativeCaciFilesystemAgent::new(PathBuf::from(project_name)))
+                cli::CaciVcsAgent::Native => {
+                    CaciConfig::new(caci_fs::config::CaciVcsAgent::Native)
                 }
             }
         },
-        CaciCommands::Init { project_name, agent } => {
+        CaciCommands::Init { ref agent } => {
             match agent {
-                CaciVcsAgent::Git => {
-                    Box::new(caci_fs::git::GitCaciFilesystemAgent::new(PathBuf::from(project_name)))
+                cli::CaciVcsAgent::Git => {
+                    CaciConfig::new(caci_fs::config::CaciVcsAgent::Git)
                 },
-                CaciVcsAgent::Native => {
-                    Box::new(NativeCaciFilesystemAgent::new(PathBuf::from(project_name)))
+                cli::CaciVcsAgent::Native => {
+                    CaciConfig::new(caci_fs::config::CaciVcsAgent::Native)
                 }
             }
         },
-    }
+        _ => {
+            CaciConfig::try_deserialize(&fs::read_to_string("caci.toml")?)?
+        }
+    };
 
     match args.command {
-        CaciCommands::Init { project_name, agent } => {
-            println!("INIT");
+        CaciCommands::New { project_name, agent } => {
+            let caci_fs_agent: Box<dyn CaciFilesystemAgent> = {
+                let repo_base_directory = PathBuf::from(project_name);
 
-            let caci_fs_agent: Box<dyn CaciFilesystemAgent> = match template {
-                Some(template) => {
-                    match template {
-                        CaciTemplate::Git => Box::new(caci_fs::git::GitCaciFilesystemAgent::new(PathBuf::from(project_name)))
-                    }
-                },
-                None => {
-                    Box::new(NativeCaciFilesystemAgent::new(PathBuf::from(project_name)))
+                match agent {
+                    cli::CaciVcsAgent::Git => Box::new(GitCaciFilesystemAgent::new(repo_base_directory, caci_config)),
+                    cli::CaciVcsAgent::Native => Box::new(NativeCaciFilesystemAgent::new(repo_base_directory, caci_config))
                 }
             };
-            
-            caci_fs_agent.initalize_caci()?;
         },
-        CaciCommands::New { project_name, template } => {
-            println!("NEW");
+        CaciCommands::Init { agent } => {
+            let caci_fs_agent: Box<dyn CaciFilesystemAgent> = {
+                let repo_base_directory = PathBuf::new();
 
-            let caci_fs_agent: Box<dyn CaciFilesystemAgent> = match template {
-                Some(template) => {
-                    match template {
-                        CaciTemplate::Git => Box::new(caci_fs::git::GitCaciFilesystemAgent::new(PathBuf::from(project_name)))
-                    }
-                },
-                None => {
-                    Box::new(NativeCaciFilesystemAgent::new(PathBuf::from(project_name)))
+                match agent {
+                    cli::CaciVcsAgent::Git => Box::new(GitCaciFilesystemAgent::new(repo_base_directory, caci_config)),
+                    cli::CaciVcsAgent::Native => Box::new(NativeCaciFilesystemAgent::new(repo_base_directory, caci_config))
                 }
             };
-
-            caci_fs_agent.initalize_caci()?;
         },
-        CaciCommands::Run { } => {
-            println!("run");
+        CaciCommands::Clean => {
+            unimplemented!();
+        },
+        CaciCommands::Write => {
+            unimplemented!();
+        },
+        CaciCommands::Hook(hook_command) => {
+            match hook_command {
+                cli::CaciHookCommands::Add { name, command } => {
+                    unimplemented!();
+                },
+                cli::CaciHookCommands::Remove { name } => {
+                    unimplemented!();
+                }
+            }
         }
     }
 
