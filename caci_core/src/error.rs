448291fd35a7b6a31b35cd5152a5d49a6bdf5bb8
declspecl <gavind2559@gmail.com>
2024-08-14 21:40:20 -0400

    [core] switch to toml_edit crate

--- caci_core/src/error.rs
+++ caci_core/src/error.rs
@@ -2,6 +2,12 @@
 pub enum CaciError {
     #[error("IOError: {0}")]
     IOError(#[from] std::io::Error),
+
+    #[error("TOMLDeserializationError: {0}")]
+    TOMLDeserializationError(#[from] toml_edit::de::Error),
+
+    #[error("TOMLSerializationError: {0}")]
+    TOMLSerializationError(#[from] toml_edit::ser::Error),
 }
 
 pub type CaciResult<T> = anyhow::Result<T, CaciError>;
